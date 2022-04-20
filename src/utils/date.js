9929a3a6ddfe752c4722995d93b6de54eaabb25d
Yan Soares <yts.1995@gmail.com>
2022-04-20 14:41:47 -0300

    refactor: change generateDate function to use date-fns

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,14 +1,6 @@
+import { addDays } from 'date-fns'
+
 export function generateDate(numberOfDays) {
-  const date = new Date()
-  const currentMonth = date.getMonth()
-  const currentMinutes = date.getMinutes()
-
-  const year = date.getFullYear()
-  const month = currentMonth < 10 ? `0${currentMonth}` : currentMonth
-  const day = date.getDate() + parseInt(numberOfDays)
-  const hour = date.getHours()
-  const minutes = currentMinutes < 10 ? `0${currentMinutes}` : currentMinutes
-  const seconds = date.getSeconds()
-
-  return new Date(year, month, day, hour, minutes, seconds)
+  const today = new Date()
+  return addDays(today, numberOfDays)
 }
